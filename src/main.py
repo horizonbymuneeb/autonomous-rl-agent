5be155527e01a77886c92c8310bbb711ec4fc7eb
horizonbymuneeb <horizonbymuneeb@gmail.com>
2026-03-04 15:58:24 +0500

    Add logging for training metrics to tensorboard

--- src/main.py
+++ src/main.py
@@ -334,3 +334,5 @@ if __name__ == '__main__':
 # Implement curriculum learning for harder tasks [2026-02-24T11:45:28]
 
 # WIP: debugging reward shaping for stability [2026-03-03T11:35:56]
+
+# Add logging for training metrics to tensorboard [2026-03-04T15:58:24]
